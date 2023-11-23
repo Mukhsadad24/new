140849bda87240d369c0c32d7f1af551caed6bc0
Mukhsadad Pratama <pratamamukhsadad@gmail.com>
2023-11-23 09:32:00 +0800

    Update1

--- main.js
+++ main.js
@@ -14,7 +14,7 @@ appreciateBtn.addEventListener("click", () => {
   myCanvas.style.inset = 0;
   myCanvas.style.width = "100vw";
   myCanvas.style.height = "100vh";
-  myCanvas.style.zIndex = -1;
+  myCanvas.style.zIndex = 3;
   document.body.appendChild(myCanvas);
 
   const myConfetti = confetti.create(myCanvas, {
